fafa634f5b2c4202b8fee9f0201c9fbb6501fd79
RenanMartins7 <71572772+RenanMartins7@users.noreply.github.com>
2023-07-30 23:14:30 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
 import 'dart:ui';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:myapp/login/manage.dart';
+import 'package:myapp/view/manage.dart';
 import 'package:myapp/utils.dart';
+import 'package:myapp/view/loginPage.dart';
 //import 'package:myapp/login/iphone-14-1.dart';
 // import 'package:myapp/login/.dart';
 //import 'package:myapp/login/iphone-14-2.dart';
@@ -13,17 +14,23 @@ import 'package:myapp/utils.dart';
 // import 'package:myapp/login/manage.dart';
 // import 'package:myapp/login/iphone-14-6.dart';
 // import 'package:myapp/login/iphone-14-9.dart';
-import 'package:myapp/login/create.dart';
+import 'package:myapp/view/create.dart';
 // import 'package:myapp/login/iphone-14-7.dart';
 // import 'package:myapp/login/iphone-14-8.dart';
 // import 'package:myapp/login/iphone-14-5.dart';
 import 'package:myapp/model/match.dart';
 import 'package:myapp/model/match_DAO_Hive.dart';
 import 'package:myapp/model/match_DAO.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
 
 void main() async {
   MatchDAO matchDAO = MatchDAOHiveImpl();
   await matchDAO.init();
+  WidgetsFlutterBinding.ensureInitialized();
+  final appDocumentDirectory = await path_provider.getApplicationDocumentsDirectory();
+  Hive.init(appDocumentDirectory.path);
+  await Hive.openBox('users');
   runApp(MyApp());
 }
 
@@ -37,7 +44,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.lightGreen,
       ),
-      home: ManagePage(),
+      home: LoginPage(),
       routes: {
         '/createMatch': (context) => CreateMatchPage(),
       },
