69fc68d20c64a9bf19cda8e7748bf7716d865cf7
RenanMartins7 <71572772+RenanMartins7@users.noreply.github.com>
2023-07-31 21:40:14 -0300

    Update matchesResultsPage.dart

--- lib/view/matchesResultsPage.dart
+++ lib/view/matchesResultsPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:myapp/model/models.dart';
+import 'package:myapp/model/daos/match_DAO.dart';
 
 class MatchResultsPage extends StatelessWidget {
   final String selectedSport;
@@ -54,17 +55,62 @@ class MatchResultsPage extends StatelessWidget {
     );
   }
 
-  Future<List<Map<String, dynamic>>> _getMatches() async {
-    final box = await Hive.openBox('matches');
-    final matches = <Map<String, dynamic>>[];
-    for (var i = 0; i < box.length; i++) {
-      final match = box.getAt(i) as Map<String, dynamic>;
-      if (match['title'] == selectedSport &&
-          match['distance'] <= maxDistance &&
-          match['positions'].contains(positions)) {
-        matches.add(match);
-      }
+Future<List<Map<String, dynamic>>> _getMatches() async {
+  await _populateMatches(); // Add sample matches to the box before getting matches
+  final box = await Hive.openBox('matches');
+  final matches = <Map<String, dynamic>>[];
+  for (var i = 0; i < box.length; i++) {
+    final match = Map<String, dynamic>.from(box.getAt(i) as Map);
+    if (match['title'] == selectedSport ||
+        (maxDistance > 0 && match['distance'] <= maxDistance) ||
+        (positions.isNotEmpty && match['positions'].contains(positions))) {
+      matches.add(match);
     }
-    return matches;
+  }
+  return matches;
+}
+
+  Future<void> _populateMatches() async {
+    final box = await Hive.openBox('matches');
+    // Example matches
+    final List<Map<String, dynamic>> exampleMatches = [
+      {
+        'title': 'futebol',
+        'firstSubtitle': 'Subtitle 1',
+        'thirdSubtitle': 'Subtitle 3',
+        'fourthSubtitle': 'Subtitle 4',
+        'distance': 10.0,
+        'positions': 'Striker, Midfielder',
+      },
+      {
+        'title': 'basquete',
+        'firstSubtitle': 'Subtitle 1',
+        'thirdSubtitle': 'Subtitle 3',
+        'fourthSubtitle': 'Subtitle 4',
+        'distance': 15.0,
+        'positions': 'Point Guard, Forward',
+      },
+      {
+        'title': 'futebol',
+        'firstSubtitle': 'Campo do Sobrado',
+        'thirdSubtitle': '15/07/2023',
+        'fourthSubtitle': 'Atacante',
+        'distance': 15.0,
+        'positions': 'Atacante, Lateral',
+      },
+      {
+        'title': 'futebol',
+        'firstSubtitle': 'Campo do Arroz',
+        'thirdSubtitle': '21/07/2023',
+        'fourthSubtitle': 'Atacante',
+        'distance': 15.0,
+        'positions': 'Atacante, Lateral',
+      }
+      // Add more example matches here
+    ];
+
+    for (final match in exampleMatches) {
+      box.add(match);
+    }
   }
 }
