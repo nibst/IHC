05bf603fa4a1ef1226b959815b943599ee2eaad8
nikolas <nikolasps7@gmail.com>
2023-08-27 15:26:06 -0300

    side menu with functionality

--- lib/view/requests.dart
+++ lib/view/requests.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
+import 'package:intl/intl.dart';
 import 'package:myapp/model/models.dart';
 import 'package:myapp/controller/controllers.dart';
 import 'package:myapp/main.dart';
@@ -14,11 +15,23 @@ class RegistrationRequestsPage extends StatefulWidget {
 
 class _RegistrationRequestsPageState extends State<RegistrationRequestsPage> {
   late List<RegistrationRequest> registrationRequests;
+  // Map to translate status values
+  Map<String, String> statusTranslations = {
+    'pending': 'pendente',
+    'accepted': 'aceito',
+    'rejected': 'rejeitado',
+  };
+
+  String translateStatus(String status) {
+    return statusTranslations[status] ?? status;
+  }
 
   @override
   Widget build(BuildContext context) {
     RegistrationRequestController controller = RegistrationRequestController(requestDAO: requestDAO);
+    MatchController matchController = MatchController(matchDAO: matchDAO);
     registrationRequests = controller.getRegistrationRequestsByUserId(currentUser.id);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Minhas Solicitações'),
@@ -28,9 +41,11 @@ class _RegistrationRequestsPageState extends State<RegistrationRequestsPage> {
         itemCount: registrationRequests.length,
         itemBuilder: (context, index) {
           final request = registrationRequests[index];
+          Match? match = matchController.getMatchById(request.matchId);
+
           return ListTile(
-            title: Text('Match ID: ${request.matchId}'),
-            subtitle: Text('Position: ${request.position}\nStatus: ${request.status}'),
+            title: Text('Esporte: ${match?.sport}'),
+            subtitle: Text('Local: ${match?.place}\nData:${DateFormat('dd-MM-yyyy HH:mm').format(match?.datetime ?? DateTime.now())} \nPosição: ${request.position}\nStatus: ${translateStatus(request.status)}'),
             trailing: IconButton(
               icon: Icon(Icons.delete),
               onPressed: () async {
--- lib/view/side_menu.dart
+++ lib/view/side_menu.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/view/profile_page.dart';
+import 'package:myapp/main.dart';
+
+import 'home.dart';
 
 class SideMenuPage extends StatelessWidget {
   @override
@@ -24,30 +28,37 @@ class SideMenuPage extends StatelessWidget {
             leading: Icon(Icons.home),
             title: Text('Menu'),
             onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => HomePage()),
+              );
               // Handle home menu option
             },
           ),
-          ListTile(
+          /*ListTile(
             leading: Icon(Icons.settings),
             title: Text('Configurações'),
             onTap: () {
               // Handle settings menu option
             },
-          ),
+          ),*/
           ListTile(
             leading: Icon(Icons.person),
             title: Text('Perfil'),
             onTap: () {
-              // Handle settings menu option
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => UserProfilePage(user: currentUser)),
+              );
             },
           ),
-          ListTile(
+          /*ListTile(
             leading: Icon(Icons.info),
             title: Text('Ajuda'),
             onTap: () {
               // Handle settings menu option
             },
-          )
+          )*/
 
           // Add more ListTile widgets for other menu options
         ],
